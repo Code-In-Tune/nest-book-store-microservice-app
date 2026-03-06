d3e730cc68c13959fc30dc0ae2f80ebb47080bae
Nicola Rossi <ncl.rss.92@gmail.com>
2026-03-06 22:05:08 +0100

    App Configuration module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { BooksModule } from './books/books.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+    }),
+    BooksModule,
+  ],
 })
 export class AppModule {}
--- src/books/books.module.ts
+++ src/books/books.module.ts
@@ -8,8 +8,13 @@ import { UpdateBookService } from './use-cases/update/update-book.service';
 import { RemoveBookService } from './use-cases/remove/remove-book.service';
 import { FindBookByIdService } from './use-cases/find-by-id/find-book-by-id.service';
 import { FindBooksService } from './use-cases/find-all/find-books.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { BookMongo, BookSchema } from './repositories/mongoose/book.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: BookMongo.name, schema: BookSchema }]),
+  ],
   controllers: [BooksController],
   providers: [
     BookMapper,
