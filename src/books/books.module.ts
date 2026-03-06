f88cfce5ace0fb30f93a94bdaaac2df197c18044
Nicola Rossi <ncl.rss.92@gmail.com>
2026-03-06 21:51:01 +0100

    BooksModule added

---
+++ src/books/books.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { BooksController } from './books.controller';
+import { BookMapper } from './mappers/book.mapper';
+import { BOOK_REPOSITORY } from './tokens/book.tokens';
+import { MongooseBookRepository } from './repositories/mongoose/mongoose-book.repository';
+import { SaveBookService } from './use-cases/save/save-book.service';
+import { UpdateBookService } from './use-cases/update/update-book.service';
+import { RemoveBookService } from './use-cases/remove/remove-book.service';
+import { FindBookByIdService } from './use-cases/find-by-id/find-book-by-id.service';
+import { FindBooksService } from './use-cases/find-all/find-books.service';
+
+@Module({
+  controllers: [BooksController],
+  providers: [
+    BookMapper,
+    {
+      provide: BOOK_REPOSITORY,
+      useClass: MongooseBookRepository,
+    },
+    SaveBookService,
+    UpdateBookService,
+    RemoveBookService,
+    FindBookByIdService,
+    FindBooksService,
+  ],
+})
+export class BooksModule {}
